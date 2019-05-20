6a0a4b9c7173b8f5d5f417fa2d78612c6b8bab10
AnLuuApp <conganwoz@gmail.com>
2019-05-20 16:55:45 +0700

    update statistic

--- mean-master/server/controllers/statistic.controller.js
+++ mean-master/server/controllers/statistic.controller.js
@@ -21,13 +21,16 @@ async function getStatistic(){
         return dateDetail;
     });
     
+    // quantity of redbook
     let num_rb = dates.length;
+    //get today date
     let dateNow = new Date();
     let day = dateNow.getDate();
     let month = dateNow.getMonth();
     let year = dateNow.getFullYear();
+
+
     let num_new_rb_today = 0;
-    let num_new_rb_month = 0;
     // dates.map((dateDetail) =>{
     //     if(dateDetail[0] === day && dateDetail[1] === month && dateDetail[2] === year){
     //         num_new_rb_today++;
@@ -37,17 +40,36 @@ async function getStatistic(){
     // });
 
 
-    num_new_rb_today = dates.filter((dateDetail) =>{
-        return parseInt(dateDetail[0]) == day && parseInt(dateDetail[1]) == month && parseInt(dateDetail[2]) == year;
-    }).length;
 
-    num_new_rb_month = dates.filter((dateDetail) =>{
-        //console.log(parseInt(dateDetail[0]) != day && parseInt(dateDetail[1]) == month && parseInt(dateDetail[2]) == year);
-        return parseInt(dateDetail[0]) != day && parseInt(dateDetail[1]) == month && parseInt(dateDetail[2]) == year;
-    }).length;
+    // statistic new rb add in range 50 day ago
+    let num_new_rb_byday = [];
+    for(let i of Array(50).keys()){
+        let d = new Date();
+        d.setDate(d.getDate() - i);
+        let obj = {
+            num_new: 0,
+            date: d,
+        };
+        num_new_rb_byday.push(obj);
+    }
+    // num_new_rb_today = dates.filter((dateDetail) =>{
+    //     return parseInt(dateDetail[0]) == day && parseInt(dateDetail[1]) == month && parseInt(dateDetail[2]) == year;
+    // }).length;
+
+
+    let i = 0;
+    num_new_rb_byday.map((item)=>{
+        num_new_rb_byday[i].num_new = dates.filter((dateDetail) =>{
+            return parseInt(dateDetail[0]) == item.date.getDate() && parseInt(dateDetail[1]) == (item.date.getMonth() + 1) && parseInt(dateDetail[2]) == item.date.getFullYear();
+        }).length;
+        i++;
+    });
 
 
 
+
+    let num_uf_enterpri = 0;
+    // find avg area of owner
     let uniqueOwner = [];
     let sumArea = 0;
     listRedBook.map((rb) =>{
@@ -55,8 +77,13 @@ async function getStatistic(){
         if(uniqueOwner.indexOf(rb.owner_id) == -1){
             uniqueOwner.push(rb.owner_id);
         }
+
+        if(rb.use_for == 'Đất xây dựng công trình'){
+            num_uf_enterpri++;
+        }
     });
     let avg_are_per_owner = sumArea/uniqueOwner.length;
+
     let top_five_dis_detail = [];
     // map to address
     let mapAddr = await Promise.all(listRedBook.map(async (rb)=>{
@@ -99,18 +126,21 @@ async function getStatistic(){
         num_rb_dis = num_rb_dis.slice(0,5);
     }
 
+    let ratio_use_for = num_uf_enterpri/listRedBook.length;
 
     return {
         num_rb,
-        num_new_rb_today,
-        num_new_rb_month,
+        ratio_use_for,
         avg_are_per_owner,
         num_rb_dis,
+        num_new_rb_byday,
     };
 
 
 
 }
+
+
 
 module.exports = {
     getStatistic,
